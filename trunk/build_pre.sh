396d28100416094df5fb69ff3c30205d8136d224
soulherats <2967964248@qq.com>
2024-03-27 05:50:07 +0000

    build before sudo bash prebuild.sh\

---
+++ trunk/build_pre.sh
@@ -0,0 +1,4 @@
+cp linux-3.4.x/include/linux/if_pppol2tp.h ../toolchain-mipsel/toolchain-3.4.x/mipsel-linux-uclibc/sysroot/usr/include/linux/if_pppol2tp.h
+cp linux-3.4.x/include/linux/if_pppox.h ../toolchain-mipsel/toolchain-3.4.x/mipsel-linux-uclibc/sysroot/usr/include/linux/if_pppox.h
+cp linux-3.4.x/include/linux/l2tp.h ../toolchain-mipsel/toolchain-3.4.x/mipsel-linux-uclibc/sysroot/usr/include/linux/l2tp.h
+cp patch/in.h ../toolchain-mipsel/toolchain-3.4.x/mipsel-linux-uclibc/sysroot/usr/include/netinet/in.h 
